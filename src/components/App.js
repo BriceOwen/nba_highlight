1b8e63fb030f1c43688e8b3281dc49f8a47342e4
BriceOwen <owenbriceind@gmail.com>
2018-02-09 14:45:41 +0300

    Header and Footer

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
+import Header from './Header';
+import Footer from './Footer';
+import Home from './Home';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        app
-      </div>
+      <BrowserRouter>
+        <div>
+          <Header/>
+          <Route exact path="/" component={Home}/>
+          <Footer/>
+        </div>
+      </BrowserRouter>
     );
   }
 }
