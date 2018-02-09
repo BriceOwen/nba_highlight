3d90df71e1b5aa823c51f6cd8d76e73372d3784f
BriceOwen <owenbriceind@gmail.com>
2018-02-09 15:43:16 +0300

    Featured

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
 
+import Featured from './Featured';
+
+
+const URL_HOME = 'http://localhost:3004/home';
 
 class Home extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            home: ''
+        }
+    }
+
+    componentDidMount() {
+        fetch(URL_HOME, {method: 'get'})
+        .then(response => response.json())
+        .then(json => {
+            this.setState({
+                home: json
+            });
+        })
+    }
+
     render() {
         return (
             <div>
-                Home
+                <Featured slides={this.state.home.slider}/>
             </div>
         )
     }
